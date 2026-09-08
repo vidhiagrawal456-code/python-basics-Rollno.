108be8be0a3b240067c1bdd7901d3743a77ca3bf
vidhiagrawal456-code <vidhiagrawal.456@gmail.com>
2026-09-08 10:54:23 +0530

    Create struct.py
    added a struct easy program to get name,age, marks

---
+++ struct.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, age, marks):
+        self.name = name
+        self.age = age
+        self.marks = marks
+
+s1 = Student("Riya", 20, 88.5)
+print(f"Name: {s1.name}, Age: {s1.age}, Marks: {s1.marks}")
